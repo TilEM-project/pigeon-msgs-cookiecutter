6b57ba2a71b7b35463fa6211a1ad65e4c57dc37a
Cameron Devine <cameron.devine@alleninstitute.org>
2024-09-19 00:10:41 -0700

    updating for the new message hashing system

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,13 +1,6 @@
 from . import example
 
-import importlib.metadata
-
-
-__version__ = importlib.metadata.version("{{ cookiecutter.pypi_name }}")
 
 topics = {
     "example.message": example.Message,
 }
-
-
-msgs = (topics, __version__)
